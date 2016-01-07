af6bfc8229b789404eeb0c4bdd3cdb4a1898c8a3
tmgree <tmgree@outlook.com>
2016-01-07 17:06:30 +0000

    done

--- thermostat_jquery.js
+++ thermostat_jquery.js
@@ -1,9 +1,19 @@
 $(document).ready(function() {
 
+  var city = $('#current-city').val();
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    $('#current-temperature').text(data.main.temp);
+  });
+
 var thermostat = new Thermostat();
 var status = thermostat.powerSavingModestatus();
 update_temperature();
-$('#power_save').text("Power Save Mode is " + status);
+$('#current-city').change(function() {
+  var city = $('#current-city').val();
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    $('#current-temperature').text(data.main.temp);
+  });
+});
 
 function update_temperature () {
   $('#temperature').text(thermostat.temperature);
