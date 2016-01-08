23eee2cbdcb3f9f66aa0040b3516ab05247052ca
tmgree <tmgree@outlook.com>
2016-01-08 15:33:24 +0000

    autocomplete bar added

--- thermostat_jquery.js
+++ thermostat_jquery.js
@@ -5,47 +5,81 @@ $(document).ready(function() {
     $('#current-temperature').text(data.main.temp);
   });
 
-var thermostat = new Thermostat();
-var status = thermostat.powerSavingModestatus();
-update_temperature();
+  var thermostat = new Thermostat();
+  var status = thermostat.powerSavingModestatus();
+  update_temperature();
 
-$('#select-city').submit(function(event) {
-  event.preventDefault();
-  var city = $('#current-city').val();
-  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
-    $('#current-temperature').text(data.main.temp);
+  $('#select-city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+      $('#current-temperature').text(data.main.temp);
+    });
   });
-});
 
-function update_temperature () {
-  $('#temperature').text(thermostat.temperature);
-  $('#thermostat').attr('class', thermostat.colour_change());
-}
+  function update_temperature () {
+    $('#temperature').text(thermostat.temperature);
+    $('#thermostat').attr('class', thermostat.colour_change());
+  }
 
-$("#temperature-up").click(function() {
-  thermostat.up();
-  update_temperature();
-});
+  $("#temperature-up").click(function() {
+    thermostat.up();
+    update_temperature();
+  });
 
-$("#temperature-down").click(function() {
-  thermostat.down();
-  update_temperature();
-});
+  $("#temperature-down").click(function() {
+    thermostat.down();
+    update_temperature();
+  });
 
-$("#temperature-reset").click(function() {
-  thermostat.resett();
-  update_temperature();
-});
+  $("#temperature-reset").click(function() {
+    thermostat.resett();
+    update_temperature();
+  });
 
-$("#powersaving-on").click(function() {
-  var status = thermostat.powerSavingModeOn();
-  $('#power_save').text("Power Save Mode is " + status );
-});
+  $("#powersaving-on").click(function() {
+    var status = thermostat.powerSavingModeOn();
+    $('#power_save').text("Power Save Mode is " + status );
+  });
 
-$("#powersaving-off").click(function() {
-  var status = thermostat.powerSavingModeOff();
-  $('#power_save').text("Power Save Mode is " + status );
-});
+  $("#powersaving-off").click(function() {
+    var status = thermostat.powerSavingModeOff();
+    $('#power_save').text("Power Save Mode is " + status );
+  });
+
+  $(function() {
+    function log( message ) {
+      $( "<div>" ).text( message ).prependTo( "#log" );
+      $( "#log" ).scrollTop( 0 );
+    }
+
+    $( "#city" ).autocomplete({
+      source: function( request, response ) {
+        $.ajax({
+          url: "http://gd.geobytes.com/AutoCompleteCity",
+          dataType: "jsonp",
+          data: {
+            q: request.term
+          },
+          success: function( data ) {
+            response( data );
+          }
+        });
+      },
+      minLength: 3,
+      select: function( event, ui ) {
+        log( ui.item ?
+          "Selected: " + ui.item.label :
+          "Nothing selected, input was " + this.value);
+        },
+        open: function() {
+          $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+        },
+        close: function() {
+          $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+        }
+      });
+    });
 
 
-});
+  });
