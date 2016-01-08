e7d527ec9b8ac3099abd4c8def95d7abfe457f61
tmgree <tmgree@outlook.com>
2016-01-08 16:07:15 +0000

    autocomplete box now functional

--- thermostat_jquery.js
+++ thermostat_jquery.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
 
   $('#select-city').submit(function(event) {
     event.preventDefault();
-    var city = $('#current-city').val();
+    var city = $('#city').val();
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
       $('#current-temperature').text(data.main.temp);
     });
